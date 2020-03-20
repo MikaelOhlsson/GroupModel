608c0bf33795bb8fd3242f0f634b12d11a562c67
Mikael Ohlsson <mikoh949@student.liu.se>
2020-03-20 10:14:13 +0100

    Review version

---
+++ code/trophic_position.R
@@ -0,0 +1,52 @@
+
+pwl <- read_delim("../data/kortsch/PairwiseList_seb.txt", delim = " ", col_names = T)
+
+for(i in distinct(pwl[,1])) {
+  print(i)
+}
+
+predators <- pwl[,1] %>% 
+  distinct()
+
+prey <- pwl[,2] %>%
+  distinct()
+
+primary <- pwl %>%
+  filter(!PREY %in% PREDATOR) %>%
+  distinct(PREY)
+
+both <- pwl %>%
+  filter(PREDATOR %in% PREY) %>%
+  distinct(PREDATOR)
+
+top <- pwl %>%
+  filter(!PREDATOR %in% PREY) %>%
+  distinct(PREDATOR)
+
+g_df$TRO_POS <- ""
+
+
+herb <- g_df %>% filter(TL > 1.1) %>% filter(TL <=2) %>% distinct(ABBREVIATION)
+
+pred <- both %>% filter(!PREDATOR %in% herb$ABBREVIATION) %>%
+  distinct(PREDATOR)
+
+for(i in 1:nrow(g_df)){
+  if(g_df[i,1] %in% primary$PREY) g_df$TRO_POS[i] <- "primary" 
+}
+
+
+for(i in 1:nrow(g_df)){
+  if(g_df[i,1] %in% herb$ABBREVIATION) g_df$TRO_POS[i] <- "herbivore" 
+}
+
+
+for(i in 1:nrow(g_df)){
+  if(g_df[i,1] %in% pred$PREDATOR) g_df$TRO_POS[i] <- "predator" 
+}
+
+
+for(i in 1:nrow(g_df)){
+  if(g_df[i,1] %in% top$PREDATOR) g_df$TRO_POS[i] <- "top" 
+}
+write.table(g_df, file="../results/data/g_df.txt", sep="\t", dec=".", quote = F, col.names = T, row.names = F)
---
+++ data/gis/readme.txt
@@ -0,0 +1,4 @@
+Subregional data of the Barents Sea, including environmental data and centres of subregions. 
+
+Obtained from:
+ Kortsch, S., Primicerio, R., Aschan, M., Lind, S., Dolgov, A.V. & Planque, B. (2018b). Food-web structure varies along environmental gradients in a high-latitude marine ecosystem. Ecography (Cop.)., 1–14.
